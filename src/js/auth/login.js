7061af51d81108f4cda1dc9faa3cbbcd001f5c04
sanDaws <castrogiraldosantiago@gmail.com>
2024-06-01 00:41:25 +0200

    fix: login functional

--- src/js/auth/login.js
+++ src/js/auth/login.js
@@ -1,45 +1,38 @@
 //our server endpoint
-const URL= 'http://localhost:3000/'
+const link = 'http://localhost:3000/'
 //form sections 
-const fomr= document.querySelector('form')
+
+const form = document.querySelector('form')
 const userEmail = form.querySelector('#email')
-const userPass=form.querySelector('#password')
+const userPass = form.querySelector('#password')
 
-async function validateMail(email) {
-    const requestRespose= await fetch(`${URL}user?email=${email}`)
-    const mails= requestRespose.json()
-    if (mails.lenght !=0) {
-        return mails[0]
-        
-    }else{
-        return false
-    }
-}
-async function validatePass(password) {
-    const requestRespose= await fetch(`${URL}user?email=${password}`)
-    const password= requestRespose.json()
-    if (password =! 0) {
-        return password[0]
-        
-    }else{
+async function validate(email, password) {
+    const requestRespose = await fetch(`${link}user?email=${email}`)
+    const datas = await requestRespose.json()
+
+    if (datas[0]['password'] === password) {
+        return datas[0]
+    } else {
         return false
     }
 
-    
+
+
 }
-form.addEventListener('submit',async (event)=>{
+
+
+form.addEventListener('submit', async (event) => {
     event.preventDefault()
-    const valmail= await validateMail(userEmail.value)
-    const valPass= await validatePass(userPass.value)
+    const valdata = await validate(userEmail.value, userPass.value)
 
-    
-    if (valPass != false && valmail != false) {
-        const jason= JSON.stringify(valmail)
-        console.log(jason);
 
-        localStorage.setItem('usertoken',jason)
-        
-    }else{
+    if (valdata) {
+
+        const jason = JSON.parse(JSON.stringify(valdata))
+
+        localStorage.setItem('usertoken', jason.id)
+        //
+    } else {
         alert('you shall no pass')
     }
 
