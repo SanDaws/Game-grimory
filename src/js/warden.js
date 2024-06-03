0b6b47565d637afb5013c85df0aec91d39a73471
Daws <castrogiraldosantiago@gmail.com>
2024-06-02 20:32:06 -0500

    logged warden verification

--- src/js/warden.js
+++ src/js/warden.js
@@ -8,4 +8,14 @@ export function guardian() {
     }
 }
 
+export function loggedWarde() {
+    const aditionButton = document.querySelector('button[type="submit"]')
+    aditionButton.addEventListener('click', (event) => {
+        const usertokken = localStorage.getItem('usertoken')
+        if (usertokken === null) {
+            event.preventDefault()
+            window.location.href = '/src/pages/auth/login.html'
 
+        }
+    })
+}
